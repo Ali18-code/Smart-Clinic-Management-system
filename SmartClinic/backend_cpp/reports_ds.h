cde6bf65d8f2f716f22072a1705cf98d58373394
Ali18-code <am6470224@gmail.com>
2025-12-23 00:17:36 +0500

    Add ReportManager class for patient reporting

--- SmartClinic/backend_cpp/reports_ds.h
+++ SmartClinic/backend_cpp/reports_ds.h
@@ -1,1 +1,24 @@
+#ifndef REPORTS_DS_H
+#define REPORTS_DS_H
 
+#include "emergency_ds.h"
+#include <vector>
+#include <string>
+
+class ReportManager {
+private:
+    int totalPatients;
+    int normalPatients;
+    int emergencyPatients;
+    std::vector<EmergencyPatient> topEmergencies;
+
+    void loadPatientData(const std::string& dbPath);
+    void loadEmergencyData(const std::string& dbPath);
+    void sortEmergencies();
+
+public:
+    ReportManager(const std::string& patientDbPath, const std::string& emergencyDbPath);
+    void generateReport(const std::string& outputPath);
+};
+
+#endif
