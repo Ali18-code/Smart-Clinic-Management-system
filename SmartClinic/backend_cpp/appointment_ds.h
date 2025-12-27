9ff18ba8554c696799a2681e81169b2450b38790
Hassanx-debug <hassanair5858@gmail.com>
2025-12-27 23:57:34 +0500

    Add Appointment data structure and queue class

--- SmartClinic/backend_cpp/appointment_ds.h
+++ SmartClinic/backend_cpp/appointment_ds.h
@@ -1,1 +1,26 @@
+#ifndef APPOINTMENT_DS_H
+#define APPOINTMENT_DS_H
 
+using namespace std;
+
+struct Appointment {
+    int patientId;
+    char patientName[50];
+    char doctor[50];
+    char time[20];
+};
+
+class AppointmentQueue {
+    Appointment arr[100];
+    int front, rear, size;
+
+public:
+    AppointmentQueue();
+    bool enqueue(Appointment a);
+    bool dequeue(Appointment &out);
+    bool isEmpty();
+    int count();
+    void toJson(char* buffer);
+};
+
+#endif
