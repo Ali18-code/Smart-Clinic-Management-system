7a588feadc1c8f001be7a291ff92835e5996e942
Ali18-code <am6470224@gmail.com>
2025-12-28 00:09:49 +0500

    Add appointment loading and saving functions

--- SmartClinic/backend_cpp/storage.h
+++ SmartClinic/backend_cpp/storage.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "patient_ds.h"
 #include "emergency_ds.h"
+#include "appointment_ds.h"
 #include <string>
 
 namespace storage {
@@ -9,4 +10,7 @@ namespace storage {
 
     bool loadEmergencyHeap(const std::string& dbPath, MaxHeap& heap);
     bool saveEmergencyHeap(const std::string& dbPath, MaxHeap& heap);
+
+    bool loadAppointments(const std::string& dbPath, AppointmentQueue& queue);
+    bool saveAppointments(const std::string& dbPath, AppointmentQueue& queue);
 }
